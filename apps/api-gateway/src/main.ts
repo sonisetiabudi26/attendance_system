8383e0183a655bb27755046239052cecfdde59c6
soni setiabudi <sonisetiabudi@sonis-MacBook-Pro.local>
2026-07-13 17:47:02 +0700

    add cors

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,28 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { ApiGatewayModule } from './api-gateway.module';
-import { AppValidationPipe } from '@attendance/common/pipes';
+import { NestFactory } from "@nestjs/core";
+import { ApiGatewayModule } from "./api-gateway.module";
+import { AppValidationPipe } from "@attendance/common/pipes";
 import {
   GrpcToHttpExceptionFilter,
   HttpExceptionFilter,
   ResponseInterceptor,
-} from '@attendance/common';
+} from "@attendance/common";
 
 async function bootstrap() {
-  const app =
-  await NestFactory.create(ApiGatewayModule);
+  const app = await NestFactory.create(ApiGatewayModule);
+  app.enableCors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
+  credentials: true,
+});
+  app.useGlobalPipes(new AppValidationPipe());
 
-app.useGlobalPipes(
-  new AppValidationPipe(),
-);
+  app.useGlobalFilters(
+    new GrpcToHttpExceptionFilter(),
+    new HttpExceptionFilter()
+  );
 
-app.useGlobalFilters(
-  new GrpcToHttpExceptionFilter(),
-  new HttpExceptionFilter(),
-);
-
-app.useGlobalInterceptors(
-  new ResponseInterceptor(),
-);
-await app.listen(3000);
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  await app.listen(3000);
 }
 bootstrap();
